cb35c494a3509aa139ca64b5fb1655c94f404ca4
rudrateja01 <rudrateja8897@gmail.com>
2025-11-03 22:19:02 +0530

    fix: correct import case for GroupList

--- src/App.jsx
+++ src/App.jsx
@@ -78,8 +78,9 @@ function App() {
               <h1>Pocket Notes</h1>
               <p> Send and receive messages without keeping your phone online.</p>
               <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+              <span className="lock">🔒end-to-end encrypted</span>
             </div>
-            <p>🔒end-to-end encrypted</p>
+            
           </div>
         )}
       </div>
